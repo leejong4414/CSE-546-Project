03f8e9c44e827dea2009ce2a89d49d2648a56c32
Jong Ho Lee <jongholee@D-10-19-113-108.dhcp4.washington.edu>
2018-11-22 16:41:11 -0800

    decirion

--- SVM.py
+++ SVM.py
@@ -15,34 +15,40 @@ def partitionData():
 
 X_train, y_train, X_test, y_test = partitionData()
 
-# SGD = SGDClassifier(loss='hinge', penalty='l2', alpha=0.0001)
+SGD = SGDClassifier(loss='hinge', penalty='l2', alpha=0.0001)
 
-# print(SGD.fit(X_train, y_train))
-# print(SGD.score(X_test, y_test))
+print(SGD.fit(X_train, y_train))
+print(SGD.score(X_test, y_test))
 
 
-# SGD2 = SGDClassifier(loss='squared_loss', penalty='l2', alpha=0.0001)
+SGD2 = SGDClassifier(loss='squared_loss', penalty='l2', alpha=0.0001)
 
-# print(SGD2.fit(X_train, y_train))
-# print(SGD2.score(X_test, y_test))
+print(SGD2.fit(X_train, y_train))
+print(SGD2.score(X_test, y_test))
 
 X_train, y_train, X_test, y_test = partitionData()
-# clf = SVC(gamma='auto')
-# print(clf.fit(X_train, y_train))
-# print(clf.score(X_test, y_test))
-
-# clf2 = SVC(gamma='auto', decision_function_shape='ovo')
-# print(clf2.fit(X_train, y_train))
-# print(clf2.score(X_test, y_test))
+clf = SVC(gamma='auto')
+print(clf.fit(X_train, y_train))
+print(clf.score(X_test, y_test))
 
 
-# clf3 = SVC(kernel='linear')
-# print(clf3.fit(X_train, y_train))
-# print(clf3.score(X_test, y_test))
+clf2 = SVC(gamma='auto', probability = True)
+print(clf2.fit(X_train, y_train))
+print(clf2.score(X_test, y_test))
 
-# clf4 = SVC(kernel='linear', decision_function_shape='ovo')
-# print(clf4.fit(X_train, y_train))
-# print(clf4.score(X_test, y_test))
+
+
+clf2 = SVC(gamma='auto', decision_function_shape='ovo')
+print(clf2.fit(X_train, y_train))
+print(clf2.score(X_test, y_test))
+
+clf3 = SVC(kernel='linear')
+print(clf3.fit(X_train, y_train))
+print(clf3.score(X_test, y_test))
+
+clf4 = SVC(kernel='linear', decision_function_shape='ovo')
+print(clf4.fit(X_train, y_train))
+print(clf4.score(X_test, y_test))
 
 clf5 = SVC(kernel='poly')
 print(clf5.fit(X_train, y_train))
@@ -56,6 +62,6 @@ clf7 = SVC(kernel='precomputed')
 print(clf7.fit(X_train, y_train))
 print(clf7.score(X_test, y_test))
 
-# clf8 = SVC(kernel='linear', decision_function_shape='ovo')
-# print(clf8.fit(X_train, y_train))
-# print(clf8.score(X_test, y_test))
+clf8 = SVC(kernel='linear', decision_function_shape='ovo')
+print(clf8.fit(X_train, y_train))
+print(clf8.score(X_test, y_test))
