43be9b6ad30232a6b3dd874daf33abcd400d5514
Eric Chan <yee96@cs.washington.edu>
2018-11-26 19:50:16 -0800

    Update'

--- Feature Engineering/randomsamply.py
+++ Feature Engineering/randomsamply.py
@@ -1,11 +1,14 @@
 import numpy as np
 import pandas as pd
 
-#/Users/Eric/Desktop/CSE-546-Project/Feature Engineering/train.csv
-X = pd.read_csv("")
 
+X = pd.read_csv("/Users/Eric/Desktop/CSE-546-Project/Feature Engineering/train.csv")
+
+X = X.as_matrix()
+print(X.shape[0])
 x = X.shape[0]
 n = X.shape[0]
+
 
 rand_idx = np.random.choice(x, (n), replace=False)
 X = X[rand_idx]
