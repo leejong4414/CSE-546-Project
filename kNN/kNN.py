ba2479c390b3301fcdfaa88576a14aa64ffcfc58
Kyle Guohao Zhang <kyleghz@Kyles-MacBook-Pro.local>
2018-11-28 02:14:18 -0800

    debug knn

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -16,13 +16,13 @@ def partitionData():
 
 X_train, y_train, X_test, y_test = partitionData()
 
-for i in range(len(10)):
+for i in range(10):
     print(i)
     model = KNeighborsClassifier(n_neighbors=(i + 1))
     # Train the model using the training sets
     model.fit(X_train, y_train)
 
     #Predict Output
-    predicted= model.predict(X_test) # 0:Overcast, 2:Mild
-    print(predicted)
+    predicted= model.predict(X_test)
+    # print(predicted)
     print(model.score(X_test, y_test))
