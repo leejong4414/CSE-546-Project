e6b898ecf763b4bd06db2253269d69ef5ce90828
Jong Ho Lee <jongholee@Jongs-MacBook-Pro.local>
2018-12-03 01:21:10 -0800

    decisoin tree fix

--- Decision Tree/DecisionTree.py
+++ Decision Tree/DecisionTree.py
@@ -14,24 +14,6 @@ def partitionData():
     return X_train[1:,:-1], X_train[1:,-1], X_test[1:, :-1], X_test[1:,-1] #X_train, Y_train, X_test, Y_test
 
 X_train, y_train, X_test, y_test = partitionData()
-
-# def partitionData():
-#     X_train = pd.read_csv('./../Feature Engineering/train_final_OHE.csv')
-#     X_test =  pd.read_csv('./../Feature Engineering/test_final_OHE.csv')
-    
-#     print(np.shape(X_train.as_matrix()))
-#     X_train.dropna()
-#     X_train = X_train.as_matrix()
-#     print(np.shape(X_train))
-    
-#     print(np.shape(X_train.as_matrix()))
-#     X_train.dropna()
-#     X_test = X_test.as_matrix()
-#     print(np.shape(X_test))
-  
-#     return X_train[:,:-1], X_train[:,-1], X_test[:, :-1], X_test[:,-1] #X_train, Y_train, X_test, Y_test
-
-X_train, y_train, X_test, y_test = partitionData()
 print(np.shape(X_train))
 print(np.shape(y_train))
 cut = int (np.shape(X_train)[0] * 0.8)
@@ -43,77 +25,84 @@ y_validation = y_train[cut:]
 y_train = y_train[:cut]
 n = 50
 
+# train_accuracy = []
+# validation_accuracy = []
 
-train_error = []
-test_error = []
-
-finalTree = tree.DecisionTreeClassifier()
-min_error = float('inf')
-min_depth = 0
-#pca
-print("Decision Tree with out pca")
-for i in tqdm(range(n)):
-    clf = tree.DecisionTreeClassifier(max_depth=(i+1))
-    clf.fit(X_train, y_train)
-    train_error.append(clf.score(X_train, y_train))
-    error = clf.score(X_validation, y_validation)
-    test_error.append(error)
-    tree.export_graphviz(clf, out_file='tree'+ str(i) + '.dot')
-    if error < min_error:
-        finalTree = clf
-        min_depth = i+1
+# finalTree = tree.DecisionTreeClassifier()
+# max_accuracy = 0
+# min_depth = 0
+# #pca
+# print("Decision Tree with out pca")
+# for i in tqdm(range(n)):
+#     clf = tree.DecisionTreeClassifier(max_depth=(i+1))
+#     clf.fit(X_train, y_train)
+#     train_accuracy.append(clf.score(X_train, y_train))
+#     accuracy = clf.score(X_validation, y_validation)
+#     validation_accuracy.append(accuracy)
+#     tree.export_graphviz(clf, out_file='tree'+ str(i) + '.dot')
+#     if accuracy > max_accuracy:
+#         max_accuracy = accuracy
+#         finalTree = clf
+#         min_depth = i+1
     
+# for i in range(n):
+#     print("depth : ", i+1)
+#     print("train error : ", train_accuracy[i])
+#     print("validation error : ", validation_accuracy[i])
 
-print("Decision Tree with out pca")
-for i in range(n):
-    print("depth : ", i+1)
-    print("train error : ", train_error[i])
-    print("validation error : ", test_error[i])
-
-print("best tree was depth ", min_depth)
-print("test error : ", finalTree.score(X_test, y_test))
+# print("+++++++++++++++++++++++++++++++++++++++++++++++")
+# print("best tree was depth ", min_depth)
+# print("test error : ", finalTree.score(X_test, y_test))
+# print("+++++++++++++++++++++++++++++++++++++++++++++++")
 
 
-train_error = []
-test_error = []
 
-finalTree = tree.DecisionTreeClassifier()
-min_error = float('inf')
-min_depth = 0
 
-print("Decision Tree with pca")
 pca = PCA()
 X_train_transformed = pca.fit_transform(X_train)
 X_test_transformed = pca.transform(X_test)
+train_accuracy = []
+validation_accuracy = []
+# print("X_test_transformed" , np.shape(X_train_transformed))
+# print("X_test_transformed", np.shape(X_test_transformed))
 
+cut = int (np.shape(X_train_transformed)[0] * 0.8)
+X_train_transformed_validation = X_train_transformed[cut:,:]
+X_train_transformed = X_train_transformed[:cut,:]
+y_validation = y_train[cut:]
+y_train = y_train[:cut]
+# print("X_train_transformed_validation",np.shape(X_train_transformed_validation))
+# print("X_train_transformed",np.shape(X_train_transformed))
+# print("y_validation",np.shape(y_validation))
+# print("y_train",np.shape(y_train))
+n = 50
+
+
+finalTree = tree.DecisionTreeClassifier()
+max_accuracy = 0
+min_depth = 0
+#pca
+print("Decision Tree with pca")
 for i in tqdm(range(n)):
     clf = tree.DecisionTreeClassifier(max_depth=(i+1))
     clf.fit(X_train_transformed, y_train)
-    train_error.append(clf.score(X_train, y_train))
-    test_error.append(clf.score(X_validation, y_validation))
-    tree.export_graphviz(clf, out_file='tree_PCA'+ str(i) + '.dot')
-    if error < min_error:
+    train_accuracy.append(clf.score(X_train_transformed, y_train))
+    accuracy = clf.score(X_train_transformed_validation, y_validation)
+    validation_accuracy.append(accuracy)
+    tree.export_graphviz(clf, out_file='tree'+ str(i) + '.dot')
+    if accuracy > max_accuracy:
+        max_accuracy = accuracy
         finalTree = clf
         min_depth = i+1
-
-
-
-print("Decision Tree with pca")
-for i in range(n):
-    print("depth : ", i+1)
-    print("train error : ", train_error[i])
-    print("validation error : ", test_error[i])
-
-print("best tree was depth ", min_depth)
-print("test error : ", finalTree.score(X_test, y_test))
     
 
+print("Decision Tree with out pca")
+for i in range(n):
+    print("depth : ", i+1)
+    print("train error : ", train_accuracy[i])
+    print("validation error : ", validation_accuracy[i])
 
-
-# bg = BaggingClassifier()
-# print(bg.fit(X_train, y_train))
-# print(bg.score(X_test, y_test))
-
-# bgb = GradientBoostingClassifier()
-# print(bgb.fit(X_train, y_train))
-# print(bgb.score(X_test, y_test))
+print("+++++++++++++++++++++++++++++++++++++++++++++++")
+print("best tree was depth ", min_depth)
+print("test error : ", finalTree.score(X_test_transformed, y_test))
+print("+++++++++++++++++++++++++++++++++++++++++++++++")
