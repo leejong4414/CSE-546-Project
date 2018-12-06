475c73408410320aa3c15a51373f9b3d2d063a67
Kyle Zhang <guohaz@rainbowdash.cs.washington.edu>
2018-12-05 21:53:29 -0800

    graph

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -11,7 +11,7 @@ from matplotlib.colors import ListedColormap
 
 def partitionData():
     print("process train")
-    X_train = np.genfromtxt("/homes/iws/guohaz/CSE546/Final_Project/train_processed.csv", delimiter=",", dtype = float)
+    X_train = np.genfromtxt("/homes/iws/guohaz/CSE546/Final_Project/forgraph.csv", delimiter=",", dtype = float)
     # X_train = np.genfromtxt("./train_processed.csv", delimiter=",", dtype=float)
     # print("process test")
     # X_test = np.genfromtxt("/homes/iws/guohaz/CSE546/Final_Project/test_final.csv", delimiter="," , dtype = float)
