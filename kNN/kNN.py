e71cdb1d0bba37a51682596a02ca04d699c31e21
Kyle Guohao Zhang <kyleghz@Kyles-MacBook-Pro.local>
2018-12-07 02:52:08 -0800

    err

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -18,7 +18,7 @@ def partitionData():
     # X_train = np.genfromtxt("train_final.csv", delimiter=",", dtype=float)
     # X_train = np.genfromtxt("./train_processed.csv", delimiter=",", dtype=float)
     print("process test")
-    X_test = np.genfromtxt("/homes/iws/ guohaz/CSE546/Final_Project/test_final.csv", delimiter="," , dtype = float)
+    X_test = np.genfromtxt("/homes/iws/guohaz/CSE546/Final_Project/test_final.csv", delimiter="," , dtype = float)
     # X_test = np.genfromtxt("test_final.csv", delimiter=",", dtype=float)
     print("finish process")
     X_train = X_train[1:, :]  # Excluse first row which is title names
