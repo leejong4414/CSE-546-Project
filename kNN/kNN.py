315a133641ff0f27b4dade85f4e2b7c786e04aeb
Kyle Guohao Zhang <kyleghz@D-10-19-64-217.dhcp4.washington.edu>
2018-12-07 16:00:09 -0800

    new

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -27,59 +27,59 @@ def partitionData():
 X_train, y_train, X_test, y_test = partitionData()
 
 
-# for i in range(10):
-#     print(i)
-#     model = KNeighborsClassifier(n_neighbors=(i + 11))
-#     # Train the model using the training sets
-#     model.fit(X_train, y_train)
-#     # model.fit(X_train_transformed, y_train)
-#
-#     #Predict Output
-#     # predicted= model.predict(X_test)
-#     # print(predicted)
-#     print(model.score(X_train, y_train))
-#     print(model.score(X_test, y_test))
-
 myList = list(range(1, 20))
 # subsetting just the odd ones
 neighbors = filter(lambda x: x % 2 != 0, myList)
 
+for i in neighbors:
+    print(i)
+    model = KNeighborsClassifier(n_neighbors=i)#, weights='distance')
+    # Train the model using the training sets
+    model.fit(X_train, y_train)
+    # model.fit(X_train_transformed, y_train)
+
+    #Predict Output
+    # predicted= model.predict(X_test)
+    # print(predicted)
+    print(model.score(X_train, y_train))
+    print(model.score(X_test, y_test))
+
 # empty list that will hold cv scores
 # cv_scores = []
 # dimension = [50, 100, 250, 500, 750]
-pca250 = PCA(n_components = 250)
-X_train_transformed = pca250.fit_transform(X_train)
-X_test_transformed = pca250.transform(X_test)
+# pca250 = PCA(n_components = 1)
+# X_train_transformed = pca250.fit_transform(X_train)
+# X_test_transformed = pca250.transform(X_test)
 # perform 10-fold cross validation
-for k in neighbors:
-    knn = KNeighborsClassifier(n_neighbors=k)
-    print(250, k)
-    scores = cross_val_score(knn, X_train_transformed, y_train, cv=10, scoring='accuracy')
-    totalscore = scores.mean()
-    print(totalscore)
+# for k in neighbors:
+#     knn = KNeighborsClassifier(n_neighbors=k)
+#     print(250, k)
+#     scores = cross_val_score(knn, X_train, y_train, cv=10, scoring='accuracy')
+#     totalscore = scores.mean()
+#     print(totalscore)
     # cv_scores.append(totalscore)
 
 
-pca500 = PCA(n_components = 500)
-X_train_transformed = pca500.fit_transform(X_train)
-X_test_transformed = pca500.transform(X_test)
-# perform 10-fold cross validation
-for k in neighbors:
-    knn = KNeighborsClassifier(n_neighbors=k)
-    print(500, k)
-    scores = cross_val_score(knn, X_train_transformed, y_train, cv=10, scoring='accuracy')
-    totalscore = scores.mean()
-    print(totalscore)
-pca750 = PCA(n_components = 750)
-X_train_transformed = pca500.fit_transform(X_train)
-X_test_transformed = pca500.transform(X_test)
-# perform 10-fold cross validation
-for k in neighbors:
-    knn = KNeighborsClassifier(n_neighbors=k)
-    print(750, k)
-    scores = cross_val_score(knn, X_train_transformed, y_train, cv=10, scoring='accuracy')
-    totalscore = scores.mean()
-    print(totalscore)
+# pca500 = PCA(n_components = 500)
+# X_train_transformed = pca500.fit_transform(X_train)
+# X_test_transformed = pca500.transform(X_test)
+# # perform 10-fold cross validation
+# for k in neighbors:
+#     knn = KNeighborsClassifier(n_neighbors=k)
+#     print(500, k)
+#     scores = cross_val_score(knn, X_train_transformed, y_train, cv=10, scoring='accuracy')
+#     totalscore = scores.mean()
+#     print(totalscore)
+# pca750 = PCA(n_components = 750)
+# X_train_transformed = pca500.fit_transform(X_train)
+# X_test_transformed = pca500.transform(X_test)
+# # perform 10-fold cross validation
+# for k in neighbors:
+#     knn = KNeighborsClassifier(n_neighbors=k)
+#     print(750, k)
+#     scores = cross_val_score(knn, X_train_transformed, y_train, cv=10, scoring='accuracy')
+#     totalscore = scores.mean()
+#     print(totalscore)
 
 # changing to misclassification error
 # MSE = [1 - x for x in cv_scores]
